89d51c7d0a4b457951c06591a90696d16d30fdbf
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-01-04 21:25:04 +0900

    斧が壊れるまでしかブロックを破壊できないような処理を追加

--- src/main/kotlin/listener/BlockBreakEventListener.kt
+++ src/main/kotlin/listener/BlockBreakEventListener.kt
@@ -1,6 +1,8 @@
 package listener
 
+import com.sun.org.apache.xpath.internal.operations.Bool
 import org.bukkit.Material
+import org.bukkit.Sound
 import org.bukkit.block.Block
 import org.bukkit.block.BlockFace
 import org.bukkit.event.EventHandler
@@ -9,6 +11,9 @@ import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
 import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.event.player.PlayerItemBreakEvent
+import org.bukkit.event.player.PlayerItemDamageEvent
+import org.bukkit.inventory.ItemStack
 import org.bukkit.metadata.FixedMetadataValue
 import org.bukkit.metadata.MetadataValue
 import org.bukkit.plugin.PluginManager
@@ -33,8 +38,11 @@ class BlockBreakEventListener(plugin: JavaPlugin) : Listener {
 
     @EventHandler(priority = EventPriority.MONITOR)
     fun onBlockBreak(event: BlockBreakEvent) {
-        if (!isLog(event.block.type)) return
+        if (!isLog(event.block)) return
+        if (!haveAxe(event.player.itemInHand)) return
 
+        var player = event.player
+        var itemStack = player.itemInHand
         var unCheckedBlocks = mutableListOf(event.block)
         var checkedBlocks = arrayListOf<Block>()
 
@@ -52,7 +60,7 @@ class BlockBreakEventListener(plugin: JavaPlugin) : Listener {
 
                     if (!checkedBlocks.contains(nextRelativeBlock)) {
                         if (!unCheckedBlocks.contains(nextRelativeBlock)) {
-                            if (isLog(nextRelativeBlock.type)) {
+                            if (isLog(nextRelativeBlock)) {
                                 unCheckedBlocks.add(nextRelativeBlock)
                             }
                         }
@@ -61,16 +69,52 @@ class BlockBreakEventListener(plugin: JavaPlugin) : Listener {
             }
         }
 
-        checkedBlocks.forEach {
-            it.breakNaturally()
+//        checkedBlocks.remove(event.block)
+
+        var durability = itemStack.durability
+        var maxDurability = itemStack.type.maxDurability
+        var expectedDurability = itemStack.durability + checkedBlocks.size
+        var itemBreakingFrag = false
+
+        if (expectedDurability >= maxDurability) {
+            itemBreakingFrag = true
+            kotlin.repeat(expectedDurability - maxDurability, {
+                checkedBlocks.removeAt(checkedBlocks.lastIndex)
+            })
+        }
+
+        checkedBlocks.forEach { it.breakNaturally(itemStack) }
+
+        itemStack.durability = durability.plus(checkedBlocks.size).toShort()
+
+        if (itemBreakingFrag) {
+            player.world.playSound(player.location, Sound.ITEM_SHIELD_BREAK, 1f, 1f)
+            player.itemInHand = ItemStack(Material.AIR)
+            player.sendMessage("アイテムが壊れた")
+        } else {
+            player.sendMessage("アイテムの耐久値: ${maxDurability - durability}" +
+                    " -> ${maxDurability - itemStack.durability}")
+        }
+
+        player.sendMessage("壊したブロック数: ${checkedBlocks.size}")
+    }
+
+    fun isLog(block: Block): Boolean {
+        when (block.type) {
+            Material.LOG   -> return true
+            Material.LOG_2 -> return true
+            else           -> return false
         }
     }
 
-    fun isLog(type: Material): Boolean {
-        when (type) {
-            Material.LOG   -> return true
-            Material.LOG_2 -> return true
-            else -> return false
+    fun haveAxe(itemStack: ItemStack): Boolean {
+        when (itemStack.type) {
+            Material.DIAMOND_AXE -> return true
+            Material.GOLD_AXE    -> return true
+            Material.IRON_AXE    -> return true
+            Material.STONE_AXE   -> return true
+            Material.WOOD_AXE    -> return true
+            else                 -> return false
         }
     }
 }
